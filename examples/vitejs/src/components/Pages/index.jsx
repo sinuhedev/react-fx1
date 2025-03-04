56755ea94f9180838ca7a36131a16c7ca57b58e1
Sinuhe Maceda <sinuhe.dev@gmail.com>
2025-03-03 23:17:37 -0600

    Added page

--- examples/vitejs/src/components/Pages/index.jsx
+++ examples/vitejs/src/components/Pages/index.jsx
@@ -7,32 +7,38 @@ export default ({ className, style }) => {
   const qs = useLocation()
 
   useEffect(() => {
-    let page = ['#/', ''].includes(qs.hash) ? 'Home' : qs.hash.substring(2)
+    ;(async () => {
+      let page = ['#/', ''].includes(qs.hash) ? 'Home' : qs.hash.substring(2)
 
-    try {
-      const path = page.split('/')
+      try {
+        const path = page.split('/')
 
-      switch (path.length) {
-        case 1:
-          page = lazy(() => import(`/app/${path[0]}/index.jsx`))
-          break
-        case 2:
-          page = lazy(() => import(`/app/${path[0]}/${path[1]}/index.jsx`))
-          break
-        case 3:
-          page = lazy(() => import(`/app/${path[0]}/${path[1]}/${path[2]}/index.jsx`))
-          break
+        switch (path.length) {
+          case 1:
+            page = await import(`/app/${path[0]}/index.jsx`)
+            break
+          case 2:
+            page = await import(`/app/${path[0]}/${path[1]}/index.jsx`)
+            break
+          case 3:
+            page = await import(`/app/${path[0]}/${path[1]}/${path[2]}/index.jsx`)
+            break
+        }
+      } catch (e) {
+        console.error(e)
+        page = await import('../../app/Http/NotFound/index.jsx')
       }
-    } catch (e) {
-      console.error(e)
-      page = lazy(() => import('../../app/Http/NotFound/index.jsx'))
-    }
 
-    startViewTransition(setPage(page), pageRef, 'fade')
+      // startViewTransition(setPage(page), pageRef, 'fade')
+
+      const Page2 = page.default
+
+      setPage(<Page2 />)
+    })()
   }, [qs.hash])
 
   return Page &&
     <div ref={pageRef} className={className} style={style}>
-      <Page ref={pageRef} />
+      {Page}
     </div>
 }

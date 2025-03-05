abc03d16786e1aeca5625993280d910fce942749
Sinuhe Maceda <sinuhe.dev@gmail.com>
2025-03-04 23:12:11 -0600

    updated

---
+++ examples/nextjs/src/components/Translation/index.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import './style.css'
+import { css } from 'react-fx1'
+import { I18n } from 'components'
+
+export default ({ children, name, value, type, className, style, readOnly, disabled, onClick = () => {} }) => {
+  return (
+    <article className={css('Translation-component', className)} style={style} name={name}>
+      Translation-component : <span><I18n value='page.user.family' /></span>
+    </article>
+  )
+}
